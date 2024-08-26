c7f2444165bf453251f6c34479694c67c2d17f99
ohmmaagoch <phatcharaphol@hotmail.com>
2024-08-26 12:42:08 +0700

    feat: simple components

--- app/src/components/Hello.vue
+++ app/src/components/Hello.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import Test from './Test.vue'
+</script>
 <template>
     <div>
         <h1>Hello props</h1>
-        <h1>{{ msg }}</h1>   
+        <h1>{{ msg }}</h1>
+        <Test msg="Hello World" />
     </div>
 </template>
 <script lang="ts">
--- app/src/components/Test.vue
+++ app/src/components/Test.vue
@@ -1,3 +1,9 @@
 <template>
     <h1>Hello test</h1>
+    <h1>{{ msg }}</h1>
 </template>
+<script setup lang="ts" >
+defineProps<{
+    msg: string
+}>()
+</script>
